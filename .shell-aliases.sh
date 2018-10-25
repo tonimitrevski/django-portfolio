22b31ff695237da3b089ff17368f138b6650a08f
Toni Mitrevski <toni@stativa.com.mk>
2018-10-25 23:05:26 +0200

    register model in admin

--- .shell-aliases.sh
+++ .shell-aliases.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+alias docker_up="docker-compose up -d"
+alias docker_down="docker-compose down"
+alias manage="docker-compose exec web python3 manage.py"
 alias makemigrations="docker-compose exec web python3 manage.py makemigrations"
 alias migrate="docker-compose exec web python3 manage.py migrate"
--- app/jobs/views.py
+++ app/jobs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Jobs
 
 
 def home(request):
-    return render(request, 'jobs/home.html')
+    job = Jobs.objects.all
+    return render(request, 'jobs/home.html', {'jobs': job})
