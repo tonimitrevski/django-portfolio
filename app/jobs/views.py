266f836c6d9b3faaf2a5b91eefad592e1133b57c
Toni Mitrevski <toni@stativa.com.mk>
2018-10-28 16:31:32 +0100

    add main page and details

--- app/jobs/views.py
+++ app/jobs/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Jobs
 
 
 def home(request):
     job = Jobs.objects.all
     return render(request, 'jobs/home.html', {'jobs': job})
+
+
+def job(request, job_id):
+    job_detail = get_object_or_404(Jobs, pk=job_id)
+    return render(request, 'jobs/detail.html', {'job': job_detail})
--- app/portfolio/urls.py
+++ app/portfolio/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import jobs.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', jobs.views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('jobs/<int:job_id>', jobs.views.job, name='job'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
